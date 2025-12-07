76069ee4385b0363359923497e9b9b0e7e1be707
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-07 10:11:02 -0500

    Update urls.py

--- salon/urls.py
+++ salon/urls.py
@@ -3,15 +3,18 @@ from . import views
 from . import api  
 
 urlpatterns = [
+    # VISTAS WEB
     path('', views.inicio, name='inicio'),
     path('dashboard/', views.dashboard_dueño, name='dashboard_dueño'),
     path('cita-confirmada/', views.cita_confirmada, name='cita_confirmada'),
     path('<slug:slug_peluqueria>/agendar/', views.agendar_cita, name='agendar_cita'),
-    path('retorno-bold/', views.retorno_bold, name='retorno_bold'), # <--- ESTA ES LA CLAVE
+    path('retorno-bold/', views.retorno_bold, name='retorno_bold'), # <--- ESTA ES OBLIGATORIA
     path('manifest.json', views.manifest_view, name='pwa_manifest'),
 
-    # API
+    # API INTERNA
     path('api/horarios/', views.obtener_horas_disponibles, name='api_horarios_web'),
+
+    # API EXTERNA
     path('api/v1/<slug:slug_peluqueria>/servicios/', api.listar_servicios, name='api_servicios'),
     path('api/v1/<slug:slug_peluqueria>/empleados/', api.listar_empleados, name='api_empleados'),
     path('api/v1/<slug:slug_peluqueria>/disponibilidad/', api.consultar_disponibilidad, name='api_disponibilidad'),
