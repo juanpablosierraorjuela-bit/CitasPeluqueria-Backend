1b4886e3092e4c71b4f56a0e3ce0c43991df7811
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-10 22:30:18 +0000

    FIX: Conectar ruta principal al inicio de la app

--- salon_project/urls.py
+++ salon_project/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    # 1. Ruta para el panel de administración
     path('admin/', admin.site.urls),
-    
-    # 2. RUTA CLAVE: Incluye todas las rutas de la aplicación 'salon' en la raíz del proyecto.
-    path('', include('salon.urls')), 
+    path('', include('salon.urls')),
 ]
