2205012923f2390107a379f17a25e18710d070d4
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-10 21:46:45 +0000

    FIX FINAL: Correccion forzada de Csrf y Middleware

--- salon/middleware.py
+++ salon/middleware.py
@@ -6,16 +6,10 @@ class PeluqueriaMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        # Lógica de seguridad:
-        # 1. Verifica si el usuario está autenticado
-        # 2. Verifica si el objeto 'user' tiene el atributo 'perfil' y luego 'peluqueria'
         if request.user.is_authenticated and hasattr(request.user, 'perfil') and request.user.perfil.peluqueria:
             set_current_tenant(request.user.perfil.peluqueria)
         else:
             set_current_tenant(None)
-
         response = self.get_response(request)
-        
-        # Limpieza final
         set_current_tenant(None)
         return response
