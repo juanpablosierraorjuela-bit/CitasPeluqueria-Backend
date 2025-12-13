aff96b1f75b9ef36cc98e6c3833f197471816778
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-12 21:36:54 -0500

    Update middleware.py

--- salon/middleware.py
+++ salon/middleware.py
@@ -1,3 +1,4 @@
+# UBICACIÓN: salon/middleware.py
 from django_multitenant.utils import set_current_tenant
 from .models import Peluqueria
 
@@ -6,10 +7,21 @@ class PeluqueriaMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        if request.user.is_authenticated and hasattr(request.user, 'perfil') and request.user.perfil.peluqueria:
-            set_current_tenant(request.user.perfil.peluqueria)
-        else:
-            set_current_tenant(None)
+        tenant = None
+        if request.user.is_authenticated:
+            try:
+                # Verificar Perfil de Dueño
+                if hasattr(request.user, 'perfil') and request.user.perfil.peluqueria:
+                    tenant = request.user.perfil.peluqueria
+                # Verificar Perfil de Empleado
+                elif hasattr(request.user, 'empleado_perfil') and request.user.empleado_perfil.peluqueria:
+                    tenant = request.user.empleado_perfil.peluqueria
+            except Exception:
+                # Ignorar errores de usuarios mal configurados para permitir que cargue el admin
+                pass
+
+        set_current_tenant(tenant)
         response = self.get_response(request)
         set_current_tenant(None)
+        
         return response
