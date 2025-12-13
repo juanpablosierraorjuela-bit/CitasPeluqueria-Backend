a7863d8b459e5e37b73badf7b37580d3f79dbb76
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-13 18:40:07 -0500

    Update apps.py

--- salon/apps.py
+++ salon/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 class SalonConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'salon'
-    verbose_name = 'Gestión de Peluquerías' # <--- ESTO CAMBIA EL TÍTULO DEL MENÚ
+    verbose_name = 'Gestión de Peluquerías'
