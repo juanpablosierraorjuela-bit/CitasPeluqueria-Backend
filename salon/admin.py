ba91c81b143062c439e40c1c49b8378f937c7860
juanpablosierraorjuela-bit <juanpablosierraorjuela@gmail.com>
2025-12-13 17:06:00 -0500

    Update admin.py

--- salon/admin.py
+++ salon/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Tenant, Professional, Service, Product, Appointment, ExternalPayment
-admin.site.register(Professional)
+
+# Registro de modelos en el panel
 admin.site.register(Tenant)
+admin.site.register(Professional)
 admin.site.register(Service)
 admin.site.register(Product)
 admin.site.register(Appointment)
 admin.site.register(ExternalPayment)
+
+# Personalización de títulos del Panel de Administración (Header y Título)
+admin.site.site_header = "Administración de Citas Peluquería"
+admin.site.site_title = "Panel de Control"
+admin.site.index_title = "Bienvenido al Sistema de Gestión"
